4c8686b83f58b6024d87f7d5eea656304dcf61d9
Joel Scoble <joel.scoble@outlook.com>
2016-09-22 16:04:09 -0500

    elide unnecessary expand func; os.ExpandEnv should be used instead

--- cfg/conf.go
+++ cfg/conf.go
@@ -1,12 +1,6 @@
 package cfg
 
-import (
-	"flag"
-	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
-)
+import "flag"
 
 // Conf is used to hold flag arguments passed on start
 type Conf struct {
@@ -31,24 +25,4 @@ func (c *Conf) Flag(s string) *flag.Flag {
 		}
 	}
 	return nil
-}
-
-// ResolveEnvVars takes a string and replaces any env vars found in the string
-// with that env vars values.  The string is split on the OS's path separator;
-// an env_var starts with a '$'.   If no env vars are present; the original
-// string will be returned.  If an error occurs while getting an env variable
-// value, it is returned.
-func ResolveEnvVars(s string) (string, error) {
-	parts := strings.Split(s, string(filepath.Separator))
-	for i, v := range parts {
-		if !strings.HasPrefix(v, "$") {
-			continue
-		}
-		val := os.Getenv(strings.TrimPrefix(v, "$"))
-		if val == "" {
-			return "", fmt.Errorf("%s: not set", v)
-		}
-		parts[i] = val
-	}
-	return filepath.Join(parts...), nil
 }
